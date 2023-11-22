900d27824e9bb6d3d350ad54ca8e4779ea58557c
nskhu12 <nskhu12@gmail.com>
2023-11-22 11:11:25 +0400

    implement copy string methods

--- WorkingWithStrings/CopyingStrings.cs
+++ WorkingWithStrings/CopyingStrings.cs
@@ -21,9 +21,11 @@ namespace WorkingWithStrings
         /// </summary>
         public static string CopyThreeChars(string source, string destination)
         {
-            // TODO #8-1. Analyze unit tests for the method, and add the method implementation.
-            // Use String.CopyTo method to copy characters: https://docs.microsoft.com/en-us/dotnet/api/system.string.copyto
-            throw new NotImplementedException();
+            char[] destinationArray = destination.ToCharArray();
+
+            source.CopyTo(0, destinationArray, 0, source.Length);
+
+            return new string(destinationArray);
         }
 
         /// <summary>
@@ -31,8 +33,11 @@ namespace WorkingWithStrings
         /// </summary>
         public static string CopyFiveChars(string source, string destination)
         {
-            // TODO #8-2. Analyze unit tests for the method, and add the method implementation.
-            throw new NotImplementedException();
+            char[] destinationArray = destination.ToCharArray();
+
+            source.CopyTo(0, destinationArray, 4, source.Length);
+
+            return new string(destinationArray);
         }
 
         /// <summary>
@@ -40,8 +45,11 @@ namespace WorkingWithStrings
         /// </summary>
         public static string CopySixChars(string source, string destination)
         {
-            // TODO #8-3. Analyze unit tests for the method, and add the method implementation.
-            throw new NotImplementedException();
+            char[] destinationArray = destination.ToCharArray();
+
+            source.CopyTo(2, destinationArray, 5, 6);
+
+            return new string(destinationArray);
         }
 
         /// <summary>
@@ -49,8 +57,14 @@ namespace WorkingWithStrings
         /// </summary>
         public static string GetProductionCode(string template, string regionCode, string locationCode, string dateCode, string factoryCode)
         {
-            // TODO #8-4. Analyze unit tests for the method, and add the method implementation.
-            throw new NotImplementedException();
+            char[] destinationArray = template.ToCharArray();
+
+            regionCode.CopyTo(1, destinationArray, 0, 1);
+            locationCode.CopyTo(4, destinationArray, 3, 2);
+            dateCode.CopyTo(3, destinationArray, 7, 3);
+            factoryCode.CopyTo(2, destinationArray, 12, 4);
+
+            return new string(destinationArray);
         }
     }
 }
